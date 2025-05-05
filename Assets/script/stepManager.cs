e1b4e95c0238168f90499daf6e8d3adfe9243825
math-pixel <mathcb74@gmail.com>
2025-05-05 18:28:56 +0200

    Add narrator

--- Assets/script/stepManager.cs
+++ Assets/script/stepManager.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor.Rendering;
 using UnityEngine;
 
-public class stepAction : MonoBehaviour
+public class stepManager : MonoBehaviour
 {
     
+    public GameObject sceneOfObject;
+    public Narateur narateur;
     public List<GameObject> listObjects;
     private GameObject currentObject;
     private int idObject = 0;
@@ -14,6 +17,7 @@ public class stepAction : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        gameObject.transform.position = sceneOfObject.gameObject.transform.position; 
         if (listObjects[idObject] != null)
         {
             currentObject = Instantiate(listObjects[0], gameObject.transform.position, Quaternion.identity);
@@ -27,9 +31,10 @@ public class stepAction : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
+            
             if (idObject + 1 < listObjects.Count)
             {
-                previousStep();
+                updateStep(1);
             }
         }
         
@@ -37,7 +42,7 @@ public class stepAction : MonoBehaviour
         {
             if (idObject - 1 >= 0)
             {
-                nextStep();
+                updateStep(0);
             }
         }
     }
@@ -51,21 +56,37 @@ public class stepAction : MonoBehaviour
         return validateState;
     }
 
-    void nextStep()
+    private void updateStep(int direction)
     {
-        Destroy(currentObject);
-        idObject--;
-        currentObject = Instantiate(listObjects[idObject], gameObject.transform.position, Quaternion.identity);
-        currentObject.transform.localScale = new Vector3(0.28f, 0.28f, 0.28f);
-        progressBar.updateState(idObject);
-    }
+        // Set the direction of the next step
+        if (direction == 0) {
+            idObject--;
+        }else if (direction == 1)
+        {
+            idObject++;
+        }
+        else
+        {
+            Debug.LogError("Error in the direction of the step is different to 0 or 1");
+        }
 
-    void previousStep()
-    {
-        Destroy(currentObject);
-        idObject++;
+        // Display the next Object
+        if (currentObject != null)
+        {
+            Destroy(currentObject);
+        }
         currentObject = Instantiate(listObjects[idObject], gameObject.transform.position, Quaternion.identity);
         currentObject.transform.localScale = new Vector3(0.28f, 0.28f, 0.28f);
         progressBar.updateState(idObject);
+
+
+        // Do other action in function of the step
+        switch (idObject)
+        {
+            case 1:
+                narateur.say("Hello World");
+                break;
+
+        }
     }
 }
